0bd9fe6ad29f2a487936a054bc76434cb49a8e46
Richard Davey <rdavey@gmail.com>
2015-11-22 02:23:18 +0000

    Adding Grayscale effect before I start to tidy up the canvas folder.

---
+++ src/canvas/effects/Grayscale.js
@@ -0,0 +1,17 @@
+import ProcessPixels from 'canvas/ProcessPixels.js';
+
+export default function Grayscale (imageData) {
+
+}
+
+    // greyScale (x, y, r, g, b, a) {
+
+    //     let avg = (r + g + b) / 3;
+
+    //     r = avg;
+    //     g = avg;
+    //     b = avg;
+
+    //     return [ r, g, b, a ];
+
+    // }
