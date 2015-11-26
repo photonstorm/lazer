aed6c49e132e66c0bc8e79fffca5ffb47af1b6b7
Richard Davey <rdavey@gmail.com>
2015-11-26 04:11:09 +0000

    Fixed the convolution filter.

--- src/canvas/filters/Convolve.js
+++ src/canvas/filters/Convolve.js
@@ -5,58 +5,53 @@ import CopyImageData from 'canvas/imagedata/CopyImageData.js';
 //   -1, 8,  -1,
 //   -1, -1, -1]
 
-export default function Convolve (imageData, weights, opaque = true) {
+export default function Convolve (imageData, weights, divisor = false, offset = 0) {
 
+    //  Copy the ImageData
     let temp = CopyImageData(imageData);
 
-    const side = Math.round(Math.sqrt(weights.length));
-    const halfSide = Math.floor(side / 2);
-
-    const src = imageData.data;
-    const sw = imageData.width;
-    const sh = imageData.height;
-
-    const w = sw;
-    const h = sh;
-
-    const dst = temp.data;
-
-    const alphaFac = (opaque) ? 1 : 0;
-
-    for (let y = 0; y < h; y++)
+    if (!divisor)
     {
-        for (let x = 0; x < w; x++)
+        divisor = weights.reduce(function(a, b) { return a + b; }) || 1;
+    }
+
+    const src = temp.data;
+    const dest = imageData.data;
+    const w = imageData.width;
+
+    let res = 0;
+
+    for (let i = 0; i < src.length; i++)
+    {
+        if ((i + 1) % 4 === 0)
         {
-            let sy = y;
-            let sx = x;
-
-            let index = (y * w + x) * 4;
-
-            let r = 0;
-            let g = 0;
-            let b = 0;
-            let a = 0;
-
-            for (let cy = 0; cy < side; cy++)
-            {
-                for (let cx = 0; cx < side; cx++)
-                {
-                    let scy = Math.min(sh - 1, Math.max(0, sy + cy - halfSide));
-                    let scx = Math.min(sw - 1, Math.max(0, sx + cx - halfSide));
-                    let srcOff = (scy * sw + scx) * 4;
-                    let wt = weights[cy * side + cx];
-                    r += src[srcOff] * wt;
-                    g += src[srcOff + 1] * wt;
-                    b += src[srcOff + 2] * wt;
-                    a += src[srcOff + 3] * wt;
-                }
-            }
-
-            imageData.data[index] = r;
-            imageData.data[index + 1] = g;
-            imageData.data[index + 2] = b;
-            imageData.data[index + 3] = a + alphaFac * (255 - a);
+            dest[i] = src[i];
+            continue;
         }
+
+        res = 0;
+
+        let pixels = [
+            src[i - w * 4 - 4] || src[i],
+            src[i - w * 4]     || src[i],
+            src[i - w * 4 + 4] || src[i],
+            src[i - 4]         || src[i],
+            src[i],
+            src[i + 4]         || src[i],
+            src[i + w * 4 - 4] || src[i],
+            src[i + w * 4]     || src[i],
+            src[i + w * 4 + 4] || src[i]
+        ];
+
+        for (let j = 0; j < 9; j++)
+        {
+            res += pixels[j] * weights[j];
+        }
+
+        res /= divisor;
+        res += offset;
+
+        dest[i] = res;
     }
 
 }
