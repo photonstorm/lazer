6074bc0f687db794d1576589a6fb1f77df3d9ccb
Richard Davey <rdavey@gmail.com>
2015-12-15 01:22:07 +0000

    Fixed IsIdentity.

--- src/math/matrix/mat23/IsIdentity.js
+++ src/math/matrix/mat23/IsIdentity.js
@@ -1,16 +1,13 @@
-let mat = Float32Array;
 
-//  Sets mat to be identity matrix
+export default function IsIdentity (dst) {
 
-export default function SetIdentity (dst) {
-
-    dst[0] = 1;
-    dst[1] = 0;
-    dst[2] = 0;
-    dst[3] = 1;
-    dst[4] = 0;
-    dst[5] = 0;
-
-    return dst;
+    return (
+        dst[0] === 1 &&
+        dst[1] === 0 &&
+        dst[2] === 0 &&
+        dst[3] === 1 &&
+        dst[4] === 0 &&
+        dst[5] === 0
+    );
 
 }
