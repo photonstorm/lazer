e64182e2b8b67c2e78b3ae2009774a295db427b5
Richard Davey <rdavey@gmail.com>
2016-01-04 23:02:09 +0000

    Have moved the Math constants to Constants.js and now reference those in various functions instead (such as PI2).

--- src/Lazer.js
+++ src/Lazer.js
@@ -1,9 +1,5 @@
 var Lazer = {
 
-    TWOPI: Math.PI * 2,
-    HALFPI: Math.PI * 0.5,
-    EPSILON: 1.0e-6,
-
     /**
     * AUTO renderer - picks between WebGL or Canvas based on device.
     * @constant
---
+++ src/math/Constants.js
@@ -0,0 +1,3 @@
+export const PI2 = Math.PI * 2;
+export const HALFPI = Math.PI * 0.5;
+export const EPSILON = 1.0e-6;
