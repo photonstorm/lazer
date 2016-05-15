79f3278654e81efd45dc6919b0e32bdc7e640833
Marc Arbesman <marbesman@gmail.com>
2016-05-15 15:18:43 -0400

    Updating import statements for Texture library

--- src/texture/default/Blank32x32.js
+++ src/texture/default/Blank32x32.js
@@ -4,7 +4,7 @@ export default function Blank32x32 () {
 
     let img = new Image();
 
-    img.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgAQMAAABJtOi3AAAAA1BMVEX///+nxBvIAAAAAXRSTlMAQObYZgAAABVJREFUeF7NwIEAAAAAgKD9qdeocAMAoAABm3DkcAAAAABJRU5ErkJggg==";
+    img.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgAQMAAABJtOi3AAAAA1BMVEX///+nxBvIAAAAAXRSTlMAQObYZgAAABVJREFUeF7NwIEAAAAAgKD9qdeocAMAoAABm3DkcAAAAABJRU5ErkJggg==';
 
     return img;
 
